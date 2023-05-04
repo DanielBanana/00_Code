cf31c4e9bf9c0b723a332c78766ab1671d7d66dd
DanielBanana <daniel.pommer@protonmail.com>
2023-05-04 13:16:58 +0200

    renamed some variables and made code more similar to simple case

--- 05_CBO/high_dimensional_cbo.py
+++ 05_CBO/high_dimensional_cbo.py
@@ -1,15 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import os
+import sys
+
+sys.path.insert(0, os.getcwd())
+from plot_results import get_plot_path
 
 # Basic implementation of the CBO Algorithm described in "A consensus-based global
 # optimization method for high dimensional machine learning problems" Section 2, Algorithm 2.1
 
-def f(x,y):
+def g(x,y):
     "Objective function"
     return (x-3.14)**2 + (y-2.72)**2 + np.sin(3*x+1.41) + np.sin(4*y-1.73)
 
-def g(x,y):
+def f(x,y):
     return (x-4)**2 + (y-4)**2
 
 # Step 0:
@@ -20,19 +25,19 @@ def g(x,y):
 dimensions = 2
 n_particles = 25
 batch_size = 10
-beta_0 = 3.0
-learning_rate_0 = 1.0# gamma
-noise_rate_0 = 0.2 # sigma
-drift_rate = 0.2 # lambda
+beta = 10.0
+gamma = 0.01 # learning_rate
+sigma = 0.1 # noise_rate
+lambda_ = 1.0 # drift_rate
 stopping_criterion = 1e-6
 particle_indices = list(range(0, n_particles))
 X = np.random.rand(n_particles, dimensions) * 5
-X = np.array(np.meshgrid(np.linspace(0,5,5), np.linspace(0,5,5))).reshape(2,25).T
+# X = np.array(np.meshgrid(np.linspace(0,5,5), np.linspace(0,5,5))).reshape(2,25).T
 
 
 remainder_set = []
 
-n_steps = 50
+n_steps = 400
 batch_centers_old = []
 batch_centers_new = []
 
@@ -53,35 +58,24 @@ ax.plot([x_min], [y_min], marker='x', markersize=5, color="white")
 contours = ax.contour(x, y, z, 10, colors='black', alpha=0.4)
 ax.clabel(contours, inline=True, fontsize=8, fmt="%.0f")
 # pbest_plot = ax.scatter(pbest[0], pbest[1], marker='o', color='black', alpha=0.5)
-p_plot = ax.scatter(X[:,0], X[:,1], marker='o', color='black', alpha=0.5)
+p_plot = ax.scatter(X[:,0], X[:,1], marker='o', color='black', alpha=0.1)
 # p_arrow = ax.quiver(X[0], X[1], V[0], V[1], color='blue', width=0.005, angles='xy', scale_units='xy', scale=1)
 # gbest_plot = plt.scatter([gbest[0]], [gbest[1]], marker='*', s=100, color='black', alpha=0.4)
 ax.set_xlim([0,5])
 ax.set_ylim([0,5])
 a = np.asarray(batch_centers_old)
-# ax.scatter(a[:,0], a[:,1])
-fig.savefig('test.png')
 
-for k in range(n_steps):
-    # Step 1 Choose Index batches
+
+def update(X, batch_centers_old, remainder_set):
+    batch_centers_new = []
     permutation = list(np.random.permutation(particle_indices))
     index_list = remainder_set + permutation
     n_batches = np.int32(np.floor((n_particles + len(remainder_set))/batch_size))
     batches = [index_list[i*batch_size:(i+1)*batch_size] for i in range(n_batches)]
     remainder_set = index_list[n_batches*batch_size:]
 
-    noise_rate = noise_rate_0/np.log(k+2)
-    beta = beta_0*np.log(k+2)
-    learning_rate = learning_rate_0*1/(k+1)
-
-    # beta = beta_0
-    # noise_rate = noise_rate_0
-    # learning_rate = learning_rate_0
-
-
-
-
     for batch in batches:
+        break_flag = False
         # Step 2.1 Calculate the function values (or approximated values) of the to be
         # minimised function at the location of the particles at the batches
         L = g(X[batch, 0], X[batch, 1])
@@ -89,15 +83,15 @@ for k in range(n_steps):
         mu = np.exp(-beta*L)
         # Expand dimension of factor mu so elementwise multiplication with X is possible
         mu = mu.reshape(-1, 1)
-        batch_center = 1/np.sum(mu) * np.sum(X[batch]*mu, 0)
+        batch_center =  np.sum(X[batch]*mu, 0)/np.sum(mu)
         # Step 2.3 Update particles
         # Calculate deviation of particles from common center from center deviation
-        unscaled_deviation = X[batch]-batch_center
-        center_deviation = drift_rate*learning_rate*unscaled_deviation
+        deviation_from_center = X[batch]-batch_center
+        consensus_term = lambda_*gamma*deviation_from_center
         # Calculate deviation from common center with random disturbance
         normal_disturbance = np.random.normal(0.0, 1.0, (batch_size, dimensions))
-        disturbance_deviation = noise_rate*np.sqrt(learning_rate)*np.sum(unscaled_deviation*normal_disturbance, 1)
-        X[batch] = X[batch] - center_deviation + disturbance_deviation.reshape(-1,1)
+        disturbance_deviation = sigma*np.sqrt(gamma)*deviation_from_center*normal_disturbance
+        X[batch] = X[batch] - consensus_term - disturbance_deviation
         batch_centers_new.append(batch_center)
     if k > 0:
         # the number of patch centers can vary from iteration to iteration, cull them
@@ -111,14 +105,31 @@ for k in range(n_steps):
         batch_center_difference = np.array(batch_centers_new[:n_batches]) - np.array(batch_centers_old[:n_batches])
         # Step 3: Check the stopping criterion
         if 1/dimensions*np.linalg.norm(batch_center_difference)**2 <= stopping_criterion:
-            break
+            break_flag = True
+
     batch_centers_old = batch_centers_new.copy()
     batch_centers_new = []
+
+    return X, batch_centers_old, remainder_set, break_flag
+
+
+for k in range(n_steps):
+    X, batch_centers_old, remainder_set, break_flag = update(X, batch_centers_old, remainder_set)
     if k%10==0:
         a = np.asarray(batch_centers_old)
-        ax.scatter(a[:,0], a[:,1], color='red', alpha = k/n_steps)
+        ax.scatter(a[:,0], a[:,1],  marker='*', color='red', alpha = k/n_steps)
         p_plot = ax.scatter(X[:,0], X[:,1], marker='o', color='black', alpha=k/n_steps*0.1)
-fig.savefig('test.png')
+    if break_flag:
+        break
+
+
+
+
+
+
+path = os.path.abspath(__file__)
+file_path = get_plot_path(path)
+fig.savefig(file_path + '.png')
 print(f'True minimum: {x_min}, {y_min}')
 print(a)
 
