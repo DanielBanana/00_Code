0193701d51d93c0cf5da25983e979192c194650c
DanielBanana <daniel.pommer@protonmail.com>
2023-07-19 15:31:22 +0200

    added shifted parabola dataset

--- 05_CBO/cbo_in_python/src/datasets.py
+++ 05_CBO/cbo_in_python/src/datasets.py
@@ -84,7 +84,7 @@ def load_generic_dataloaders(train_dataset:GenericDataset, train_batch_size:int,
     test_dataloader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=shuffle, drop_last=drop_last)
     return train_dataloader, test_dataloader
 
-def load_parabola_dataloaders(train_batch_size, test_batch_size, data_dir=None, n_train_samples=60000, n_test_samples=10000):
+def load_parabola_dataloaders(train_batch_size, test_batch_size, data_dir=None, n_train_samples=60000, n_test_samples=10000, shuffle=True):
     x_train = torch.Tensor(np.random.uniform(-5, 5, n_train_samples)).reshape(-1,1)
     x_test = torch.Tensor(np.random.uniform(-5, 5, n_test_samples)).reshape(-1,1)
 
@@ -93,10 +93,27 @@ def load_parabola_dataloaders(train_batch_size, test_batch_size, data_dir=None, 
 
     y_train = f(x_train)
     train_dataset = GenericDataset(x_train, y_train)
-    train_dataloader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=shuffle)
 
     y_test = f(x_test)
     test_dataset = GenericDataset(x_test, y_test)
-    test_dataloader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=shuffle)
+
+    return train_dataloader, test_dataloader
+
+def load_shifted_parabola_dataloaders(train_batch_size, test_batch_size, data_dir=None, n_train_samples=60000, n_test_samples=10000, shuffle=True):
+    x_train = torch.Tensor(np.random.uniform(-5, 5, n_train_samples)).reshape(-1,1)
+    x_test = torch.Tensor(np.random.uniform(-5, 5, n_test_samples)).reshape(-1,1)
+
+    def f(x):
+        return 8.53*(1-x*x)
+
+    y_train = f(x_train)
+    train_dataset = GenericDataset(x_train, y_train)
+    train_dataloader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=shuffle)
+
+    y_test = f(x_test)
+    test_dataset = GenericDataset(x_test, y_test)
+    test_dataloader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=shuffle)
 
     return train_dataloader, test_dataloader
